013047a63fac3e9c40d263cbfd77c0ddf0846c44
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-01-07 21:35:20 +0630

    Making to my application for connecting with mongodb.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { useState } from "react";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-export default Signup
+  const onSignup = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign Up</h1>
+      <hr />
+      <label htmlFor="username">UserName</label>
+      <input
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-x-gray-600"
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-x-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-x-gray-600"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button onClick={onSignup} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        Sign Up
+      </button>
+      <Link href="/login">Login</Link>
+    </div>
+  );
+};
+
+export default Signup;
