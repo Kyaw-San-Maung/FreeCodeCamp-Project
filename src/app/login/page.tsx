04eb05a8e42fd1244c4b77a3eee8303fb360d620
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-01-09 20:36:51 +0630

    signup and login creation is completed by Kyaw San Maung.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,8 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-
-
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -28,20 +27,19 @@ const LoginPage = () => {
     try {
       setLoading(true);
       const response = await axios.post("/api/users/login", user);
-      console.log(response);
-      toast.success("Login Success");
-      useRouter().push("/profile")
-      
+      console.log("Login Successful.", response.data);
+      router.push("/profile");
+      console.log("Login successfully done.");
     } catch (error: any) {
       console.log("Login failed", error.message);
-      toast.error(error.message)
+      toast.error(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>{ loading ? "Processing..." : "Login Form"}</h1>
+      <h1>{loading ? "Processing..." : "Login Form"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input
