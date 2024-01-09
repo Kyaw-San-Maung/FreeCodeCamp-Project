0afff2e658226280e91b6c6515d07d499334b7a4
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-01-10 02:14:05 +0630

    Creating middleware for auth part of our app.

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath:boolean = path == '/login' || path == '/signup';
+
+    const token:string = request.cookies.get("token")?.value || '';
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/login", "/profile", "/signup"]
+};
